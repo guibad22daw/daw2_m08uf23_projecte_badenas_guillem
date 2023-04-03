8f7e4d1239d8028e88dce3c3b7c0fecaf52dd196
guibad22daw <15587039.clot@fje.edu>
2023-04-03 15:16:12 +0200

    Visualitzar dades d'usuari completat

--- menu.php
+++ menu.php
@@ -1,24 +1,25 @@
 <?php
-    session_start();
-    error_reporting(0);
-    if (!isset($_SESSION['usuari'])) {
-        header("Location: errors/error_acces.php");
-    } else if (!isset($_SESSION['expira']) || (time() - $_SESSION['expira'] >= 0)) {
-        header("Location: errors/logout_expira_sessio.php");
-    }
+	session_start();
+	error_reporting(0);
+	if (!isset($_SESSION['usuari'])) {
+		header("Location: errors/error_acces.php");
+	} else if (!isset($_SESSION['expira']) || (time() - $_SESSION['expira'] >= 0)) {
+		header("Location: errors/logout_expira_sessio.php");
+	}
 ?>
 <html>
 
 <head>
-    <title>
-        Menú principal | Gestió LDAP
-    </title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<title>
+		Menú principal | Gestió LDAP
+	</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+		integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" href="css/menu.css">
 </head>
 
 <body>
-<h2 class="titol">Menú principal</h2>
+	<h2 class="titol">Menú principal</h2>
 	<div class="card-group">
 		<div class="card">
 			<img class="card-img-top" src="/assets/img/vis_usuaris.webp" alt="Card image cap">
@@ -35,7 +36,7 @@
 				<p class="card-text">Creació d'un nou usuari a la base de dades LDAP.</p>
 				<a href="afegir_usuari.php" class="btn btn-light">Anar</a>
 			</div>
-		</div>		
+		</div>
 	</div>
 	<div class="card-group">
 		<div class="card">
@@ -45,7 +46,7 @@
 				<p class="card-text">Modificació de les dades d'un usuari de la base de dades LDAP.</p>
 				<a href="modificar_usuari.php" class="btn btn-light">Anar</a>
 			</div>
-		</div>		
+		</div>
 		<div class="card">
 			<img class="card-img-top" src="/assets/img/esborra_usuaris.png" alt="Card image cap">
 			<div class="card-body">
--- visualitzar_usuari.php
+++ visualitzar_usuari.php
@@ -22,8 +22,10 @@ if (!isset($_SESSION['usuari'])) {
         $ldap->bind();
         $entrada = 'uid=' . $_GET['usr'] . ',ou=' . $_GET['ou'] . ',dc=fjeclot,dc=net';
         $usuari = $ldap->getEntry($entrada);
-        if($usuari) $visualitza = 1;
-        else $visualitza = 2;
+        if ($usuari)
+            $visualitza = 1;
+        else
+            $visualitza = 2;
     }
 }
 ?>
@@ -33,30 +35,40 @@ if (!isset($_SESSION['usuari'])) {
     <title>
         MOSTRANT DADES D'USUARIS DE LA BASE DE DADES LDAP
     </title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/visualitzar_usuari.css">
 </head>
 
 <body>
-    <h2>Formulari de selecció d'usuari</h2>
-    <form action="visualitzar_usuari.php" method="GET">
-        Unitat organitzativa: <input type="text" name="ou"><br/><br/>
-        Usuari: <input type="text" name="usr"><br><br/>
-        <input type="submit" />
-        <input type="reset" />
-    </form>
-    <br />
+    <h2 class="titol">Visualitzar dades d'usuari</h2>
+    <div class="form input-group">
+        <h4 for="basic-url">Introdueix les dades necessàries</h4><br />
+        <form action="visualitzar_usuari.php" method="GET">
+            Unitat organitzativa: <input type="text" name="ou" class="form-control"
+                placeholder="Unitat organitzativa"><br />
+            Nom de l'usuari: <input type="text" name="usr" class="form-control" placeholder="usuari"><br>
+            <div style="display: flex; justify-content: center">
+                <button class="btn btn-primary">Consultar</button>
+            </div>
+        </form>
+    </div>
+    <br /><br />
     <?php
-        if ($$visualitza == 1) {
-            echo "<b><u>" . $usuari["dn"] . "</b></u><br>";
-            foreach ($usuari as $atribut => $dada) {
-                if ($atribut != "dn")
-                    echo $atribut . ": " . $dada[0] . '<br>';
-            }
-        } else if ($visualitza == 2) {
-            echo "<script type='text/javascript'>alert('Aquest usuari no existeix. Comprova bé les dades.');</script>";
+    if ($visualitza == 1) {
+        echo '<table class="table table-striped table-bordered"><thead><tr><th scope="col">Nom complet</th><th scope="col">Descripció</th><th scope="col">GID</th><th scope="col">Nom de pila</th><th scope="col">Directori personal</th><th scope="col">Shell</th><th scope="col">Núm. de mòbil</th><th scope="col">Object Class</th><th scope="col">Adreça</th><th scope="col">Cognom</th><th scope="col">Núm de telefon</th><th scope="col">Títol</th><th scope="col">UID</th><th scope="col">Número de UID</th></tr></thead><tbody><tr>';
+        foreach ($usuari as $atribut => $dada) {
+            if ($atribut != "dn")
+                echo "<td>$dada[0]</td>";
         }
+        echo "</tr></tbody></table><br/><br/>";
+    } else if ($visualitza == 2) {
+        echo "<script type='text/javascript'>alert('Aquest usuari no existeix. Comprova bé les dades.');</script>";
+    }
     ?>
-    <br/>
-    <a href="menu.php">Tornar al menú</a>
+    <div style="display: flex; justify-content: center">
+        <a class="btn btn-danger" style="width: 10%" href="menu.php">Tornar al menú principal</a>
+    </div>
 </body>
 
 </html>
