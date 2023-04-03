3530ba1b2457faec843c377b1740a1ece0737aa4
guibad22daw <15587039.clot@fje.edu>
2023-04-03 02:02:49 +0200

    Pàgina login feta

--- index.php
+++ index.php
@@ -1,15 +1,54 @@
 <html>
-	<head>
-		<title>
-			PÀGINA WEB INICIAL DE L'APLICACIÓ
-		</title>
-	</head>
-	<body>
-		<h1> APLICACIÓ D'ACCÉS A BASES DE DADES LDAP</h1>
-		<h2> DAW2 M08UF2 M08UF3 </h2>
-		<h3> Autor: dacomo2021daw2</h3>
-		<h3> Correu: dacomo2021daw2@protonmail.com</h3>
-		<h3> Github: https://github.com/guibad2daw/</h3>
-		<a href="login.php">Inicia sessió</a>
-	</body>
+
+<head>
+	<title>
+		Pàgina inicial
+	</title>
+</head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
+	integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+<style>
+	* {
+		box-sizing: border-box;
+		padding: 5;
+	}
+
+	.container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 100%;
+	}
+
+	body {
+		background-image: url('assets/img/data_warehouse.png');
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+
+	.centered-element {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		color: white;
+	}
+
+	.centered-element h1 {
+		font-weight: bold;
+		font-size: 55px;
+	}
+</style>
+
+<body>
+	<div class="container">
+		<div class="centered-element">
+			<h1> APLICACIÓ D'ACCÉS A BASES DE DADES LDAP</h1>
+			<h3> Consulta, afegeix, edita i esborra usuaris de la base de dades LDAP.</h3>
+			<a class="btn btn-primary" href="login.php">Inicia sessió</a>
+		</div>
+	</div>
+</body>
+
 </html>
--- login.php
+++ login.php
@@ -1,43 +1,48 @@
 <?php
-    require 'vendor/autoload.php';
-	use Laminas\Ldap\Ldap;
+require 'vendor/autoload.php';
+use Laminas\Ldap\Ldap;
 
-	ini_set('display_errors', 0);
-	if ($_POST['cts'] && $_POST['adm']){
-	   $opcions = [
-            'host' => 'zend-gubabe.fjeclot.net',
-		    'username' => "cn=admin,dc=fjeclot,dc=net",
-   		    'password' => 'fjeclot',
-   		    'bindRequiresDn' => true,
-		    'accountDomainName' => 'fjeclot.net',
-   		    'baseDn' => 'dc=fjeclot,dc=net',
-       ];	
-	   $ldap = new Ldap($opcions);
-	   $dn='cn='.$_POST['adm'].',dc=fjeclot,dc=net';
-	   $ctsnya=$_POST['cts'];
-	   try{
-	       $ldap->bind($dn,$ctsnya);
-           session_start();
-           $_SESSION['usuari'] = $_POST['adm'];
-           $_SESSION['expira'] = time() + 216000;
-	       header("location: menu.php");
-	   } catch (Exception $e){
-	       echo "<b>Contrasenya incorrecta</b><br><br>";	       
-	   }
+ini_set('display_errors', 0);
+if ($_POST['cts'] && $_POST['adm']) {
+	$opcions = [
+		'host' => 'zend-gubabe.fjeclot.net',
+		'username' => "cn=admin,dc=fjeclot,dc=net",
+		'password' => 'fjeclot',
+		'bindRequiresDn' => true,
+		'accountDomainName' => 'fjeclot.net',
+		'baseDn' => 'dc=fjeclot,dc=net',
+	];
+	$ldap = new Ldap($opcions);
+	$dn = 'cn=' . $_POST['adm'] . ',dc=fjeclot,dc=net';
+	$ctsnya = $_POST['cts'];
+	try {
+		$ldap->bind($dn, $ctsnya);
+		session_start();
+		$_SESSION['usuari'] = $_POST['adm'];
+		$_SESSION['expira'] = time() + 216000;
+		header("location: menu.php");
+	} catch (Exception $e) {
+		echo "<b>Contrasenya incorrecta</b><br><br>";
 	}
+}
 ?>
 <html>
-	<head>
-		<title>
-			AUTENTICANT AMB LDAP DE L'USUARI admin
-		</title>
-	</head>
-	<body>
-		<form action="login.php" method="POST">
-			Usuari amb permisos d'administració LDAP: <input type="text" name="adm"><br>
-			Contrasenya de l'usuari: <input type="password" name="cts"><br>
-			<input type="submit" value="Envia" />
-			<input type="reset" value="Neteja" />
-		</form>
-	</body>
+
+<head>
+	<title>
+		Login
+	</title>
+	<link rel="stylesheet" href="stylesheets/login.css">
+</head>
+
+<div class="login">
+	<h1>Inicia sessió</h1>
+    <form method="post">
+    	<input type="text" name="adm" placeholder="Usuari" required="required" />
+        <input type="password" name="cts" placeholder="Contrasenya" required="required" />
+        <button type="submit" class="btn">Inicia sessió</button>
+    </form>
+</div>
+</body>
+
 </html>
